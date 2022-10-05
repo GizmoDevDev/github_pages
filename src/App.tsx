746255eef729aa8a875c811c1626b058282f5336
Gizmo <gizmo.dev.dev@gmail.com>
2022-10-05 21:37:02 +0600

    change main page

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2> Deploy from branch</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
